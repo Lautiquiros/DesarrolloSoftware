11408a4b1e4944138ea0c6996c9d7a3db375ddec
Lautiquiros <143513609+Lautiquiros@users.noreply.github.com>
2024-09-21 21:26:37 -0300

    Delete TP-APIREST directory

--- TP-APIREST/src/main/java/com/example/apirest/repositories/LocalidadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.apirest.repositories;
-
-import com.example.apirest.entities.Localidad;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocalidadRepository extends BaseRepository<Localidad, Long>{
-}
--- TP-APIREST/src/main/java/com/example/apirest/services/AutorService.java
+++
@@ -1,7 +0,0 @@
-package com.example.apirest.services;
-
-import com.example.apirest.entities.Autor;
-import com.example.apirest.entities.Base;
-
-public interface AutorService extends BaseService<Autor, Long> {
-}
